6b88f97f04e2e1cacae155947acdecbe5119b338
JuneLin2001 <asd900606@gmail.com>
2023-10-25 17:59:04 +0800

    display image

--- script.js
+++ script.js
@@ -18,20 +18,3 @@ document.getElementById("button3").addEventListener("click", function () {
     document.getElementById("image2").style.display = "none";
     document.getElementById("image3").style.display = "block";
 });
-
-
-function previewImage() {
-    const imageInput = document.getElementById("imageInput");
-    const imagePreview = document.getElementById("imagePreview");
-    const previewImage = document.getElementById("previewImage");
-
-    if (imageInput.files.length > 0) {
-        const imageFile = imageInput.files[0];
-        const imageURL = URL.createObjectURL(imageFile);
-
-        previewImage.src = imageURL;
-        imagePreview.style.display = "block";
-    } else {
-        imagePreview.style.display = "none";
-    }
-}
